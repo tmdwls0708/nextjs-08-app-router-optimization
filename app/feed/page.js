07b29c05d46b6e3313e704784a1d53a503c7ff63
sjkim <07nevergiveup08@gmail.com>
2025-12-12 15:48:05 +0900

    페이지에서 메타데이터 설정 추가

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,14 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+export async function generateMetadata(params) {
+  const posts = await getPosts();
+  const num = posts.length;
+  return {
+    title: `Browse all our ${num} posts`,
+    description: "Browse All out posts",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
