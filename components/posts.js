271a3d5c2953f57eb2e3572c9b7b2b4854de4727
sjkim <07nevergiveup08@gmail.com>
2025-12-12 15:34:12 +0900

    클라우디너리 옵션으로 이미지 크기를 서버에서 수정해서 가져오므로 Image 컴포넌트의 fill 속성을 없애고 width, height 추가

--- components/posts.js
+++ components/posts.js
@@ -21,7 +21,8 @@ function Post({ post, action }) {
         <Image
           loader={imageLoader}
           src={post.image}
-          fill
+          width={200}
+          height={100}
           alt={post.title}
           quality={50}
         />
